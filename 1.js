f1711b886013ac956d964ce7c94b0043685cdeaf
nargis21 <nargisakther1933@gmail.com>
2024-01-02 22:38:00 +0600

    problem-1 soltion-3 added

--- 1.js
+++ 1.js
@@ -1,14 +1,24 @@
 const arr = [1, 5, 9, 14, 5, 22, 48, 25, 22, 20, 9, 13];
 
-// How to remove duplicate elements from this array ?
+// Q: How to remove duplicate elements from this array ?
 
+//solution-1
 // const uniqueArray = [... new Set(arr)]
 
-const uniqueArray = arr.filter((value, index, self) => {
-    return self.indexOf(value) === index;
-})
+//solution-2
+// const uniqueArray = arr.filter((value, index, self) => {
+//     return self.indexOf(value) === index;
+// })
 
-console.log(uniqueArray);
+//solution-3
+// const uniqueArray = arr.reduce((accumulator, currentValue) => {
+//     if (!accumulator.includes(currentValue)) {
+//         accumulator.push(currentValue)
+//     }
+//     return accumulator
+// }, [])
+
+// console.log(uniqueArray);
 
 // How to find out the largest / lowest elements from this array ?
 
