bf7fbfd577a948294e14608260f767b18c3d0ea7
nargis21 <nargisakther1933@gmail.com>
2024-01-02 22:07:45 +0600

    first commit

---
+++ 1.js
@@ -0,0 +1,11 @@
+const arr = [1, 5, 9, 14, 5, 22, 48, 25, 22, 20, 9, 13];
+
+// How to remove duplicate elements from this array ?
+
+const uniqueArray = [... new Set(arr)]
+console.log(uniqueArray);
+
+
+// How to find out the largest / lowest elements from this array ?
+
+// How to slice(22, 48) from this array
