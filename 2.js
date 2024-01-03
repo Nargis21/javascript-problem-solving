3d0278b108f57dab4b4ebeff72d0a83f4914d310
nargis21 <nargisakther1933@gmail.com>
2024-01-03 16:09:04 +0600

    general mathmatical problem solved

--- 2.js
+++ 2.js
@@ -9,3 +9,22 @@
 // This is brilliant!
 // I wish you’ll shine in your life!	Good luck with all the barriers(/\) in your life.`)
 
+//Q-2 তোমাকে দুইটি ইন্টিজার ভ্যেলু ইনপুট নিতে হবে এবং তাদের যোগফল, বিয়োগফল, গুণফল এবং ভাগফল নীচে দেখানো ফরম্যাট এ দেখাতে হবে।
+
+
+// For example:
+// Inputs are 5 and 2
+// Then you’ll give output as:
+// 5 + 2 = 7
+// 5 - 2 = 3
+// 5 * 2 = 10
+// 5 / 2 = 2.50
+
+//solution
+const calculator = (num1, num2) => {
+    console.log(num1, '+', num2, "=", num1 + num2);
+    console.log(num1, '-', num2, "=", num1 - num2);
+    console.log(num1, '*', num2, "=", num1 * num2);
+    console.log(num1, '/', num2, "=", num1 / num2);
+}
+calculator(1, 2)
